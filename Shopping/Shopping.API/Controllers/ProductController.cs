922cc48480c0a671155a7a188a179cd11ea27dfb
Usman Tahir <Usman.tahir@synergy-it.com>
2022-10-03 22:59:21 +0500

    Create Product Controller Class for Shopping.API Microservice

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shopping.API.Models;
+
+namespace Shopping.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Product
+            {
+               Name="asd"
+            })
+            .ToArray();
+        }
+
+       
+    }
+}
